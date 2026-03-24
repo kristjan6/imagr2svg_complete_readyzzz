eea1d8639f7218c4f89965d205df995b7e092f5f
kristjan6 <81045579+kristjan6@users.noreply.github.com>
2026-03-24 02:48:15 +0100

    Delete blueprint2dwg_complete_readyzzz directory

--- blueprint2dwg_complete_readyzzz/start.ps1
+++
@@ -1,4 +0,0 @@
-if (!(Test-Path .env)) {
-  Copy-Item .env.example .env
-}
-docker compose up --build
--- blueprint2dwg_complete_readyzzz/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ ! -f .env ]; then
-  cp .env.example .env
-fi
-docker compose up --build
